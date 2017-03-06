0eed049739b572560e7683fded47a2862d777046
Daniel West <daniel@silverback.is>
2017-03-06 20:33:09 +0000

    Stop smoking page front-end

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,14 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('@App/Default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('@App/Default/index.html.twig');
+    }
+
+    /**
+     * @Route("/stop-smoking", name="stop_smoking_choices")
+     */
+    public function stopSmokingAction(Request $request)
+    {
+        return $this->render('@App/Default/stop_smoking.html.twig');
     }
 }
---
+++ src/AppBundle/Resources/assets/local/js/stop-smoking-choices.js
@@ -0,0 +1,34 @@
+var $selectedTab;
+function positionArrow()
+{
+  if($selectedTab)
+  {
+    var $parent = $selectedTab.parent(),
+    newLeft = $parent.index()*33.33;
+    $("#selectedArrow").css({
+      left: newLeft+"%",
+      marginLeft: ($selectedTab.outerWidth()/2)-($("#selectedArrow").width()/2)
+    });
+  }
+}
+
+$(function(){
+  $('.nav-item .btn-cta').on('show.bs.tab', function (e) {
+    //e.target // newly activated tab
+    //e.relatedTarget // previous active tab
+    
+    if(typeof e.relatedTarget == 'undefined')
+    {
+      //no tab previous showing, hide the captions, show the arrow
+      $(".arrow-row").css({display: 'flex'});
+      $(".chance-percent-row").hide();
+    }
+    $selectedTab = $(this);
+    positionArrow();    
+  });
+  $(window).on("resize orientationchange", positionArrow);
+
+  $(".card-outline-success").each(function(){
+    $(this).clone().addClass("mb-5").appendTo($(this).parent());
+  });
+});
