c272cf95f91e5ab4631912844ae954b08bfee2f2
Daniel West <daniel@silverback.is>
2017-03-07 19:16:04 +0000

    Borough search attempt 1

--- src/AppBundle/Resources/assets/global/GoogleMap/GoogleMap.js
+++ src/AppBundle/Resources/assets/global/GoogleMap/GoogleMap.js
@@ -1,45 +1,205 @@
-var GoogleMap = (function($){
-  var public = {
-    init: function(){
-      var map = new google.maps.Map(document.getElementById('map'), {
+var GoogleMap = (function($, viewport, alert, confirm){
+  var bootstrap4Divs = {
+    'xs': $('<div class="device-xs d-block hidden-sm-up"></div>'),
+    'sm': $('<div class="device-sm d-block hidden-xs-down hidden-md-up"></div>'),
+    'md': $('<div class="device-md d-block hidden-sm-down hidden-lg-up"></div>'),
+    'lg': $('<div class="device-lg d-block hidden-md-down hidden-xl-up"></div>'),
+    'xl': $('<div class="device-xl d-block hidden-lg-down"></div>')
+  };    
+  viewport.use('bootstrap4', bootstrap4Divs);
+
+  var map,
+  newJSON = {
+    "type": "FeatureCollection",
+    "features": []
+  },
+  feature = {
+    "type": "Feature",
+    "properties": {
+      "name": null
+    },
+    "geometry": {}
+  },
+  colors = ['#1866b8', '#0a76e5', '#31a9ff', '#b2d6fa'],//
+  geoJSON,
+  center = {lat: 51.4820, lng: -0.09},
+  postcodeRegExp = new RegExp("[A-Za-z]{1,2}[0-9Rr][0-9A-Za-z]? ?([0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2})?"),
+  searchMarker = null,
+  selectedFeature = null,
+  public = {
+    initialised: false,
+    init: function()
+    {
+      map = new google.maps.Map(document.getElementById('map'), {
         zoom: 10,
-        center: {lat: 51.5074, lng: 0.1278},
-        mapTypeId: 'terrain'
+        center: center,
+        styles: [
+          {
+            "featureType": "all",
+            "stylers": [
+              { 
+                "color": "#eceeef",
+                "visibility": "off"
+              }
+            ]
+          }
+        ],
+        //mapTypeControl: false,
+        mapTypeId: 'roadmap',
+        //streetViewControl: false,
+        zoomControl: false,
+        clickableIcons: false,
+        disableDefaultUI: true,
+        draggable: false,
+        gestureHandling: "none",
+        keyboardShortcuts: false,
+        scrollwheel: false
       });
 
-      //var myParser = new geoXML3.parser({map: map});
-      //myParser.parse('/bundles/app/maps/LondonBoroughs.kml');
-      var boroughsLayer = new google.maps.KmlLayer({
-        url: 'https://www.stopsmokingportal.com/bundles/app/maps/LondonBoroughs.kml',
-        map: map
+      // http://stackoverflow.com/questions/3081021/how-to-get-the-center-of-a-polygon-in-google-maps-v3/13772082#13772082
+      google.maps.Polygon.prototype.poly_getBounds = function(){
+        var bounds = new google.maps.LatLngBounds();
+        this.getPath().forEach(function(element,index){bounds.extend(element);});
+        return bounds;
+      };
+
+      map.data.loadGeoJson('/bundles/app/maps/GeoJson.json', {}, function(features){
+        //console.log(feature);
+        map.data.setStyle({
+          fillColor: colors[1],
+          fillOpacity: 1,
+          strokeWeight: 1,
+          strokeColor: '#FFFFFF',
+          strokeOpacity: 1,
+          draggable: false,
+          clickable: false
+        });
+        $.each(features, function(index){
+          var colIndex = index%colors.length;
+          var initStrkIndex = colIndex===1 ? index+1 : index+3;
+          var strkIndex = initStrkIndex%colors.length;
+          var featureStyle = {
+            fillColor: colors[colIndex],
+            strokeColor: colors[strkIndex],
+            strokeWeight: 1,
+            zIndex: 1
+          };
+          map.data.overrideStyle(this, featureStyle);
+          this.setProperty('originalColors', featureStyle);
+        });
+        public.initialised = true;
       });
 
-      /*var ctaLayer = new google.maps.KmlLayer({
-        url: 'http://googlemaps.github.io/js-v2-samples/ggeoxml/cta.kml',
-        map: map
-      });*/
+      $("#confirmPostcode").on("click", function(e){
+        e.preventDefault();
+        var postcode = $("#postcodeInput").val();
+        if(!postcodeRegExp.test(postcode))
+        {
+          alert("Please enter a valid UK postcode", {
+            title: 'Invalid postcode'
+          });
+          return;
+        }
+        else
+        {
+          var geocoder = new google.maps.Geocoder();
+          geocoder.geocode({address: postcode, region: 'GB'}, function(results, status) 
+          {   
+            // callback with a status and result
+            if (status == google.maps.GeocoderStatus.OK) 
+            {
+              var resultLatLng = new google.maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng());
+              
+              // Unset any selected feature
+              if(selectedFeature)
+              {
 
-      /*
-      // Define the LatLng coordinates for the polygon's path.
-      var triangleCoords = [
-        {lat: 25.774, lng: -80.190},
-        {lat: 18.466, lng: -66.118},
-        {lat: 32.321, lng: -64.757},
-        {lat: 25.774, lng: -80.190}
-      ];
-
-      // Construct the polygon.
-      var bermudaTriangle = new google.maps.Polygon({
-        paths: triangleCoords,
-        strokeColor: '#FF0000',
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: '#FF0000',
-        fillOpacity: 0.35
+                map.data.overrideStyle(selectedFeature, selectedFeature.getProperty('originalColors'));
+                selectedFeature = null;
+              }
+              // Unset the marker if already present
+              if(searchMarker)
+              {
+                searchMarker.setMap(null);
+              }
+              // Loop through all features
+              map.data.forEach(function(feature){
+                // Get geometry lat lng array from feature
+                var geometry = feature.getGeometry(),
+                latLngArray = geometry.getArray()[0].getArray();
+                // create a temporary polygon becayse we cannot access a polygon class it seems from a feature
+                var tempPolygon = new google.maps.Polygon({paths: latLngArray});
+                // check if the found marker point is within the new polygon
+                if(google.maps.geometry.poly.containsLocation(resultLatLng, tempPolygon))
+                {
+                  // The marker is within the polygon, set it as selected and stop looping
+                  selectedFeature = feature;
+                  map.data.overrideStyle(selectedFeature, {
+                    fillColor: '#15ce75',
+                    strokeColor: '#FFFFFF',
+                    strokeWeight: 4,
+                    zIndex: 2
+                  });
+                  return false;
+                }
+              });
+              // Check if the marker point is within any borough, if not display error
+              if(!selectedFeature)
+              {
+                alert("Sorry, the address entered does not appear to be within the London boroughs. Please try again.");
+                $("#selectedBorough").html("Not Found");
+                $("#boroughCard").hide();
+              }
+              else
+              {
+                // Add the marker at the new position
+                searchMarker = new google.maps.Marker({
+                  position: resultLatLng,
+                  map: map,
+                  animation: google.maps.Animation.DROP
+                });
+                // Update and show the card displaying the borough
+                $("#selectedBorough").html(selectedFeature.getProperty('name'));
+                $("#boroughCard").show();
+              }
+            }
+            else
+            {
+              alert("Sorry, there was an error looking up that postcode. Please check it and try again.");
+            }
+          });
+        }
       });
-      bermudaTriangle.setMap(map);
-      */
+
+      $(function(){
+        var $map = $('#map'),
+        updateMap = function(breakpoint)
+        {
+          switch(breakpoint)
+          {
+            case "xl":
+              $map.css({
+                height: '500px',
+                width: '630px'
+              });
+              google.maps.event.trigger(map, "resize");
+              map.setZoom(10);
+            break;
+            default:
+              $map.css({
+                height: '280px',
+                width: '312px'
+              });
+              google.maps.event.trigger(map, "resize");
+              map.setZoom(9);
+            break;
+          }
+          map.setCenter(center);
+        };
+        viewport.breakpointChanged(updateMap);
+        updateMap(viewport.current());
+      });
     }
-  };
+  };  
   return public;
-})(jQuery);
+})(jQuery, ResponsiveBootstrapToolkit, BootstrapModalAlerts.alert, BootstrapModalAlerts.confirm);
