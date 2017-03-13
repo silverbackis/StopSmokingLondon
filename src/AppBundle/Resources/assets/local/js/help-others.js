5305451f171d6b1fda8b34f034540c27b95333b3
Daniel West <daniel@silverback.is>
2017-03-13 17:21:49 +0000

    Center LinkedIn Popup

--- src/AppBundle/Resources/assets/local/js/help-others.js
+++ src/AppBundle/Resources/assets/local/js/help-others.js
@@ -11,7 +11,15 @@ $(function(){
   $("#shareL").on("click", function(e)
   {
     e.preventDefault();
-    window.open(this.href,'targetWindow', 'toolbar=no, location=no, status=no, menubar=no, resizable=no, width=520, height=570');
+    var w = 520,
+    h = 570;
+
+    var wLeft = window.screenLeft ? window.screenLeft : window.screenX;
+    var wTop = window.screenTop ? window.screenTop : window.screenY;
+
+    var left = wLeft + (window.innerWidth / 2) - (w / 2);
+    var top = wTop + (window.innerHeight / 2) - (h / 2);
+    window.open(this.href,'targetWindow', 'toolbar=no, location=no, status=no, menubar=no, resizable=no, width=' + w + ', height=' + h + ',left=' + left + ',top=' + top + ',screenX=' + left + ',screenY=' + top);
   });
 
   var clipboard = new Clipboard('#shareLink');
