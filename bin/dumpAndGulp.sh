36d3c8277c35be1f9b037bac3d74d95778eb4763
Daniel West <daniel@silverback.is>
2017-03-17 10:23:44 +0000

    composer update - generate optimised autoload post-install and post-update

--- bin/dumpAndGulp.sh
+++ bin/dumpAndGulp.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-composer dump-autoload --optimize --no-dev --classmap-authoritative
 cd "${BASH_SOURCE%/*}" || exit
 php ./console cache:clear -e prod --no-warmup
 rm -rf ./../src/AppBundle/Resources/assets/assetic/*
