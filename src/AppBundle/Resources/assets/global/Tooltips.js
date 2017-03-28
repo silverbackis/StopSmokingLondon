563efac7c3abe5b3aa687a0cedea275ca8805512
Daniel West <daniel@silverback.is>
2017-03-28 13:31:28 +0100

    Javascript and outdated browser notices

--- src/AppBundle/Resources/assets/global/Tooltips.js
+++ src/AppBundle/Resources/assets/global/Tooltips.js
@@ -92,3 +92,46 @@ var Tooltips = (function($){
     refresh: addLinks
   };
 })(jQuery);
+
+(function(){
+  function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+      window.onload = func;
+    } else {
+      window.onload = function() {
+          if (oldonload) {
+              oldonload();
+          }
+          func();
+      };
+    }
+  }
+
+  $(function () {
+    //call plugin function after DOM ready
+    addLoadEvent(function(){
+      outdatedBrowser({
+        lowerThan: 'flex'
+      });
+    });
+
+    //https://v4-alpha.getbootstrap.com/getting-started/browsers-devices/#internet-explorer-10-in-windows-phone-8
+    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+      var msViewportStyle = document.createElement('style');
+      msViewportStyle.appendChild(
+        document.createTextNode(
+          '@-ms-viewport{width:auto!important}'
+        )
+      );
+      document.head.appendChild(msViewportStyle);
+    }
+
+    //https://v4-alpha.getbootstrap.com/getting-started/browsers-devices/#android-stock-browser
+    var nua = navigator.userAgent;
+    var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1);
+    if (isAndroid) {
+      $('select.form-control').removeClass('form-control').css('width', '100%');
+    }
+  });
+})();
